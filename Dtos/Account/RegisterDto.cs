31cf0d3fabc4dd7cdd1c7e17638283c10b3b60cc
JuanXRossi <juancruzrossi3009@gmail.com>
2026-05-26 12:22:28 -0300

    Add error messages for attirbute validation

--- Dtos/Account/RegisterDto.cs
+++ Dtos/Account/RegisterDto.cs
@@ -4,17 +4,17 @@ namespace SmartFridgeAPI.Dtos.Account
 {
     public class RegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
         public string? Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El email es obligatorio")]
         [EmailAddress]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
         public string? Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         [RegularExpression(@"^[A-Z][a-z]+(\s[A-Z][a-z]+)*$")]
         public string? Name { get; set; }
     }
