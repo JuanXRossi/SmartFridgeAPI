0b0e7c30fde617f6797ecbf8ae1a49d45447a69f
JuanXRossi <juancruzrossi3009@gmail.com>
2026-08-18 16:49:20 -0300

    Add roles to login response for role-based home page redirection

--- Dtos/Account/NewUserDto.cs
+++ Dtos/Account/NewUserDto.cs
@@ -4,6 +4,7 @@ namespace SmartFridgeAPI.Dtos.Account
     {
         public string UserName { get; set; }
         public string Email { get; set; }
+        public List<string>? Roles { get; set; }
         public string Token { get; set; }
         public string RefreshToken { get; set; }
     }
