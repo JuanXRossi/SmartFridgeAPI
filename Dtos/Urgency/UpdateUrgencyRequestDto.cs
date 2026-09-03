43915e3db40cce8a426cb6d3ba5718374a628ef4
JuanXRossi <juancruzrossi3009@gmail.com>
2026-09-03 16:51:01 -0300

    Modify min length for urgency names

--- Dtos/Urgency/UpdateUrgencyRequestDto.cs
+++ Dtos/Urgency/UpdateUrgencyRequestDto.cs
@@ -5,12 +5,12 @@ namespace SmartFridgeAPI.Dtos.Urgency
     public class UpdateUrgencyRequestDto
     {
         [Required(ErrorMessage = "El nombre es obligatorio")]
-        [MinLength(5, ErrorMessage = "El nombre tiene que tener al menos 5 caracteres")]
+        [MinLength(4, ErrorMessage = "El nombre tiene que tener al menos 4 caracteres")]
         [MaxLength(20, ErrorMessage = "El nombre tiene que tener a lo sumo 20 caracteres")]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "La mínima cantidad es obligatoria")]
-        [Range(0, 4, ErrorMessage = "La mínima cantidad tiene que ser un valor entre 0 y 4")]
+        [Range(1, 10, ErrorMessage = "La mínima cantidad tiene que ser un valor entre 0 y 4")]
         public int MinAmount { get; set; }
     }
 }
